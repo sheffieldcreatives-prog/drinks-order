5e03724c6a9096598ef2871140f9e32bf12e689a
sheffieldcreatives-prog <sheffieldcreatives@gmail.com>
2025-10-23 12:43:45 +0100

    Update firebase-config.js

--- docs/firebase-config.js
+++ docs/firebase-config.js
@@ -1,21 +1,23 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDs__o-OanlqbDAbIXa1J8oJxPtxLmapW8",
-  authDomain: "drinks-or.firebaseapp.com",
-  projectId: "drinks-or",
-  storageBucket: "drinks-or.firebasestorage.app",
-  messagingSenderId: "352500209261",
-  appId: "1:352500209261:web:ccde9ccb286cd8353b9164",
-  measurementId: "G-R48G73RJGG"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDs__o-OanlqbDAbIXa1J8oJxPtxLmapW8",
+    authDomain: "drinks-or.firebaseapp.com",
+    projectId: "drinks-or",
+    storageBucket: "drinks-or.firebasestorage.app",
+    messagingSenderId: "352500209261",
+    appId: "1:352500209261:web:ccde9ccb286cd8353b9164",
+    measurementId: "G-R48G73RJGG"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
