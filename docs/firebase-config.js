3aafeb241834bee8d075cec5cd47087c7b47d62a
sheffieldcreatives-prog <sheffieldcreatives@gmail.com>
2025-10-23 12:37:57 +0100

    Update firebase-config.js

--- docs/firebase-config.js
+++ docs/firebase-config.js
@@ -1,16 +1,3 @@
-// Replace the placeholder values below with your Firebase project config.
-// You can find these in the Firebase Console: Project settings -> Your apps -> SDK config
-// Example:
- const firebaseConfig = {
-  apiKey: "AIzaSyDs__o-OanlqbDAbIXa1J8oJxPtxLmapW8",
-  authDomain: "drinks-or.firebaseapp.com",
-  projectId: "drinks-or",
-  storageBucket: "drinks-or.firebasestorage.app",
-  messagingSenderId: "352500209261",
-  appId: "1:352500209261:web:ccde9ccb286cd8353b9164",
-  measurementId: "G-R48G73RJGG"
-};
-//
 // Save this file as public/firebase-config.js before deploying.
 
 const firebaseConfig = {
